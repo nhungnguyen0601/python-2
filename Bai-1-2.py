afe8eda81e4d90bc48d70c21a64cb3a7cdcce221
Nhung Nguyen <nhungnguyentester@gmail.com>
2024-09-12 21:53:58 +0700

    first commit-nhung

---
+++ Bai-1-2.py
@@ -0,0 +1,10 @@
+#Bài 1: In các số từ 1 đến 10 bằng vòng lặp for.
+# var_range = range(1, 11)
+# for i in var_range:
+#     print(i)
+
+#Bai 2: In các số từ 1 đến 10 bằng vòng lặp while.
+# i = 1
+# while i <=10:
+#     print(i)
+#     i +=1
---
+++ bai-3.py
@@ -0,0 +1,5 @@
+#Bài 3: Tính tổng các số từ 1 đến 100 bằng vòng lặp for.
+var_range = 0
+for i in range(1,5):
+    var_range +=i
+print(var_range)
