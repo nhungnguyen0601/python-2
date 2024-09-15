e489c6bb425f8f93129e3b2fbff2dbc51c6baf99
Nhung Nguyen <nhungnguyentester@gmail.com>
2024-09-15 21:09:55 +0700

    `1

--- Bai-4.py
+++ Bai-4.py
@@ -1,1 +1,10 @@
-##test
+
+#Bài 4: Tìm các số chia hết cho 3 từ 1 đến 50 bằng vòng lặp while.
+i = 1
+while i <= 50:
+    if i % 3 ==0:
+        print(i)
+    i +=1
+
+    
+
--- Bai-5.py
+++ Bai-5.py
@@ -11,3 +11,4 @@ list_2 =[1,2,3,4,5,6,7,8,9,10]
 for i in list_1:
     for j in list_2:
         print(f"{i} x {j} = {i*j}")
+        
