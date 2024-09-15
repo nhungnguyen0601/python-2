67a01a530c42302f5b878e5d1b1994c5d702cbb9
Nhung Nguyen <nhungnguyentester@gmail.com>
2024-09-15 22:08:33 +0700

    2nd_cmt

---
+++ bai-7.py
@@ -0,0 +1,21 @@
+# Dự án: Quản lý danh sách học sinh
+# Yêu cầu: Viết chương trình cho phép người dùng thêm, xóa, và in danh sách học sinh bằng vòng lặp.
+hocsinh =[]
+while True:
+    print("1. Them: \n2. Xoa: \n3. Hien thi ds: \n4. Thoat:")
+    nhap = int(input ("Chon thao tac:"))
+    if nhap ==1:
+      name = input("Them hoc sinh:")
+      hocsinh.append(name)
+    elif nhap ==2:
+        name = input("Xoa hoc sinh: ")
+        hocsinh.remove(name)
+    elif nhap ==3:
+        for hs in hocsinh:
+            print(f"{hs}")
+    elif nhap ==4:
+        print("Thoat")
+        break
+    else:
+        print("Error")
+
