05a5eba49d0bc183eeab5c93bc69acfc3291e783
Nhung Nguyen <nhungnguyentester@gmail.com>
2024-09-15 21:06:13 +0700

    cmt_15-sep

---
+++ Bai-5.py
@@ -0,0 +1,13 @@
+#Bài 5: Viết chương trình in ra bảng cửu chương từ 2 đến 9.
+# number = int(input("Bang cuu chuong: "))
+
+# if number< 2 or number >9:
+#     print(" Error")
+# else:
+#     for i in range (2,10):
+#         print(f"{number} x {i} = {number*i}")
+list_1 =[2,3,4,5,6,7,8,9]
+list_2 =[1,2,3,4,5,6,7,8,9,10]
+for i in list_1:
+    for j in list_2:
+        print(f"{i} x {j} = {i*j}")
